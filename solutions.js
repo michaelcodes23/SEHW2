e81cc36dc0010ae8591cc27fd429ecd135dc76f9
michaelcodes <michaelletsgo@Michaels-MacBook-Pro.local>
2021-03-11 23:55:45 -0500

    prime numbers completed

--- solutions.js
+++ solutions.js
@@ -29,3 +29,34 @@ function sumArray (arr){
 
 console.log(sumArray([10,32,4,4]));
 
+console.log(Math.sqrt(36));
+
+function checkPrime (num){
+    
+    if(num <= 1){
+        return false
+    }
+    else if(num === 2){
+        return true;
+    }
+    else {
+        for(let i = 2; i < num; i ++){
+            if(num % i === 0){
+            return false
+            }
+            else if (num == Math.pow(i,2)){
+            return false
+            }
+        }
+    return true
+    }
+}
+console.log(checkPrime(37))
+function printPrimes (num){
+    const arrPrime = [];
+    for (let i = 0; i <= num; i++){
+        if (checkPrime(i)) arrPrime.push(i);
+    }
+    return arrPrime
+}
+console.log(printPrimes(97));
