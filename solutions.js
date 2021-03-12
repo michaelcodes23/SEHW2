b66e1412363555fe8d5e933c110c7393cf76977f
michaelcodes <michaelletsgo@Michaels-MacBook-Pro.local>
2021-03-11 22:01:16 -0500

    verbal questions completed

--- solutions.js
+++ solutions.js
@@ -1,1 +1,8 @@
-console.log("Starting a new file");
+
+//WRITTEN QUESTIONS 
+//1. In a funciton, parameters are the values defined when starting the function (e.g.
+//function parameters (parameter1,parameter2)). Arguments are the values that go inside
+//when calling a function (e.g. parameters(value1,value2))
+//2. Console.log is a function itself that allows you to debugg your code, a return in a function
+//allows you output the code in the function 
+
